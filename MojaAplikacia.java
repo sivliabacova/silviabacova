b3a9cdd718d108e72fb933d5f6d7e6dddfaf2f0b
Silvia Bacova <132918438+sivliabacova@users.noreply.github.com>
2023-06-15 14:16:29 +0200

    Add files via upload

---
+++ MojaAplikacia.java
@@ -0,0 +1,25 @@
+package mojaaplikacia;
+
+
+public class MojaAplikacia {
+    
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("retazec");
+        int cislo = 10;
+        
+        zmen(s);
+        cislo = zmen(cislo);
+        
+        System.out.println(s);
+        System.out.println(cislo);
+    }
+    
+    public static void zmen(StringBuilder s){
+        s.append(" ahoj");
+    }
+    
+    public static int zmen(int cislo){
+        cislo += 5;
+        return cislo; 
+    }
+}
